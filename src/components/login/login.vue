b1bf59644952647dfc5096e142da8393edfc0599
my1241402012 <1241402012@qq.com>
2018-04-12 20:23:09 +0800

    导入element组件库并简单布局登录页

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>
-      <p>登录组件</p>
+  <div id="login-wrap">
+    <el-form label-position="top" ref="form" :model="userForm" label-width="80px">
+      <el-form-item label="用户名">
+        <el-input v-model="userForm.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input type="password" v-model="userForm.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="login">立即登录</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data () {
-      return {}
+    return {
+      userForm: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    async login () {
+      const res = await axios.post('http://localhost:8888/api/private/v1/login', this.userForm)
+      const data = res.data
+      if (data.meta.status === 200) {
+        this.$router.push({
+          name: 'home'
+        })
+      }
+    }
   }
 }
 </script>
